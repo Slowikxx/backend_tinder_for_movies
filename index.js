6a36057fff2e46bdb944b65ffb7f3d77b12794cc
Slowikxx <aniakslowik@student.agh.edu.pl>
2024-10-27 14:59:31 +0100

    - added logic for making a PUT call to the backend on accepting or rejecting the movie recommendation

--- index.js
+++ index.js
@@ -23,6 +23,80 @@ app.get('/recommendations', (req, res) => {
 	});
 });
 
+app.put('/recommendations/:id/accept', (req, res) => {
+	const recommendationId = req.params.id;
+
+	fs.readFile(__dirname + '/' + 'recommendations.json', 'utf8', (err, data) => {
+		if (err) {
+			return res
+				.status(500)
+				.send('Error retrieving data from recommendations file');
+		}
+
+		let recommendations = JSON.parse(data);
+
+		const currentRecommendation = recommendations.find(
+			(recommendation) => recommendation.id === recommendationId
+		);
+
+		if (!currentRecommendation) {
+			return res.status(404).send('Recommendation of given id not found');
+		}
+
+		currentRecommendation.status = 'accepted';
+
+		fs.writeFile(
+			__dirname + '/' + 'recommendations.json',
+			JSON.stringify(recommendations),
+			'utf8',
+			(err) => {
+				if (err) {
+					return res.status(500).send('Error updating recommendation status');
+				}
+				console.log('Recommendation accepted');
+				res.status(200).send('Recommendation status updated successfully');
+			}
+		);
+	});
+});
+
+app.put('/recommendations/:id/reject', (req, res) => {
+	const recommendationId = req.params.id;
+
+	fs.readFile(__dirname + '/' + 'recommendations.json', 'utf8', (err, data) => {
+		if (err) {
+			return res
+				.status(500)
+				.send('Error retrieving data from recommendations file');
+		}
+
+		let recommendations = JSON.parse(data);
+
+		const currentRecommendation = recommendations.find(
+			(recommendation) => recommendation.id === recommendationId
+		);
+
+		if (!currentRecommendation) {
+			return res.status(404).send('Recommendation of given id not found');
+		}
+
+		currentRecommendation.status = 'rejected';
+
+		fs.writeFile(
+			__dirname + '/' + 'recommendations.json',
+			JSON.stringify(recommendations),
+			'utf8',
+			(err) => {
+				if (err) {
+					return res.status(500).send('Error updating recommendation status');
+				}
+				console.log('Recommendation rejected');
+				res.status(200).send('Recommendation status updated successfully');
+			}
+		);
+	});
+});
+
 app.listen(PORT_NUMBER, () => {
 	console.log(`Srver is running on port ${PORT_NUMBER}`);
 });
