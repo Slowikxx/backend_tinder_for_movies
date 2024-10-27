4aa1a3b0c1166e366ab39c0b48b7592293581e7e
Slowikxx <aniakslowik@student.agh.edu.pl>
2024-10-27 14:53:04 +0100

    - added getting all recommendations by GET call to the backend

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.get('/', (req, res) => {
 	res.send('Backend is running');
 });
 
+app.get('/recommendations', (req, res) => {
+	fs.readFile(__dirname + '/' + 'recommendations.json', 'utf8', (err, data) => {
+		if (err) {
+			return res
+				.status(500)
+				.send('Error retrieving data from recommendations file');
+		}
+
+		res.status(200).send(data);
+	});
+});
+
 app.listen(PORT_NUMBER, () => {
 	console.log(`Srver is running on port ${PORT_NUMBER}`);
 });
